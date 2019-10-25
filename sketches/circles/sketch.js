1908d5c1450765e2c3cf3fe7e8dba029a87ae795
David Omar Flores Chávez <david@davidomar.com>
2019-10-25 03:54:09 -0500

    Update Circles example

--- sketches/circles/sketch.js
+++ sketches/circles/sketch.js
@@ -1,7 +1,7 @@
 /**
  * Standard function of p5js
  */
-RANDOM_S = Math.random()
+RANDOM_S = Math.random() * 1000000000
 
 function setup() {
   createCanvas(DIMENSIONS[0], DIMENSIONS[1]).parent("artwork-container");
@@ -18,9 +18,11 @@ function draw() {
   background(225);
   randomSeed(RANDOM_S)
   for(let i = 0; i < NUMBER_POINTS; i++){
+    let radius = randomGaussian(Number(CIRCLE_SIZE_AVG), Number(CIRCLE_SIZE_SD))
     stroke(random(255))
     fill(random(255))
     strokeWeight(random(5))
-    circle(random(width), random(height), random(20))
+    circle(random(width), random(height), radius)
+
   }
 }
