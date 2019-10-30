0e39e5b7c38d6ee1c329d3a841d0e42283789581
David Omar Flores Chávez <david@davidomar.com>
2019-10-29 22:49:38 -0600

    Update default values for nematodes

--- sketches/nematodes/sketch.js
+++ sketches/nematodes/sketch.js
@@ -1,7 +1,5 @@
-
-RANDOM_S = Math.random() * 1000000000
-NOISE_S = Math.random() * 1000000000
-
+RANDOM_S = Math.random() * 1000000000;
+NOISE_S = Math.random() * 1000000000;
 
 /**
  * Standard function of p5js
@@ -9,69 +7,65 @@ NOISE_S = Math.random() * 1000000000
 function setup() {
   createCanvas(DIMENSIONS[0], DIMENSIONS[1]).parent("artwork-container");
 
-  randomSeed(RANDOM_S)
-  noiseSeed(NOISE_S)
+  randomSeed(RANDOM_S);
+  noiseSeed(NOISE_S);
   // Call draw() only once
   // noLoop();
 }
-z = 0
+z = 0;
 /**
  * Standard function of p5js
  */
 function draw() {
   z++;
   // Set a background color
-  colorMode(HSL)
+  colorMode(HSL);
   background(MAIN_HUE, 10, 90);
+  strokeWeight(STROKE_WEIGHT);
 
+  let CELL_SIZE = [width / MAZE_CELLS_W, height / MAZE_CELLS_H];
 
-  let CELL_SIZE = [
-    (width / MAZE_CELLS_W),
-    (height / MAZE_CELLS_H)
-  ]
+  let field = [];
 
-  let field = []
-
-  for(let i = 0; i < MAZE_CELLS_W; i++){
-    field.push([])
-    for(let j = 0; j < MAZE_CELLS_H; j++){
+  for (let i = 0; i < MAZE_CELLS_W; i++) {
+    field.push([]);
+    for (let j = 0; j < MAZE_CELLS_H; j++) {
       field[i].push(
-        noise(i * NOISE_SCALE, j * NOISE_SCALE, z * NOISE_SCALE * 0.5)
-      )
+        noise(i * NOISE_SCALE, j * NOISE_SCALE, z * CHANGE_SPEED * 0.5)
+      );
     }
   }
 
-  
+  for (let i = 0; i < field.length; i++) {
+    for (let j = 0; j < field[i].length; j++) {
+      let h = field[i][j];
+      if (
+        random() > DRAWING_PROBABILITY ||
+        h > Number(MAX_HEIGHT) ||
+        h < Number(MIN_HEIGHT)
+      ) {
+        continue;
+      }
 
-  for(let i = 0; i < field.length; i++){
-    for(let j = 0; j < field[i].length; j++){
-      stroke(MAIN_HUE, 20, 20)
-      let h = field[i][j]
-
+      stroke(MAIN_HUE, 20, 20);
       let mazeLine = [
-        (i+1) * CELL_SIZE[0], j * CELL_SIZE[1],
-        (i) * CELL_SIZE[0], (j+1) * CELL_SIZE[1]
-      ]
-      if (random() > 0.5){
+        (i + 1) * CELL_SIZE[0],
+        j * CELL_SIZE[1],
+        i * CELL_SIZE[0],
+        (j + 1) * CELL_SIZE[1]
+      ];
+      if (random() > 0.5) {
         mazeLine = [
-          i * CELL_SIZE[0], j * CELL_SIZE[1],
-          (i+1) * CELL_SIZE[0], (j+1) * CELL_SIZE[1]
-        ]
+          i * CELL_SIZE[0],
+          j * CELL_SIZE[1],
+          (i + 1) * CELL_SIZE[0],
+          (j + 1) * CELL_SIZE[1]
+        ];
       }
 
-      sw = Number(STROKE_WEIGHT) * field[i][j]
-      strokeWeight(STROKE_WEIGHT)
+      if (random() > 0.5) stroke(MAIN_HUE, 40, 50);
 
-      if (random() > Number(BASE_PROBABILITY) + field[i][j] *  field[i][j]){
-        continue;
-      }
-      if (random() > 0.5)
-      stroke(MAIN_HUE, 40, 50)
-
-      if (h > Number(MAX_HEIGHT) || h < Number(MIN_HEIGHT)){
-        continue;
-      }
-      line(mazeLine[0], mazeLine[1], mazeLine[2], mazeLine[3])
+      line(mazeLine[0], mazeLine[1], mazeLine[2], mazeLine[3]);
     }
   }
 }
